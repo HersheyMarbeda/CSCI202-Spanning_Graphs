f67f1f46734d869491369f5d34520af9aaf6713f
Hershey Marbeda <marbedahershey@gmail.com>
2024-12-04 15:29:33 -0500

    Initial Commit

---
+++ Spanning_Graphs/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Spanning_Graphs
+//
+//  Created by Hershey Marbeda on 12/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
